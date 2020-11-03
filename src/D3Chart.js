fa5fcb40d10e6d8d6b80d803ed956a4e32a1da83
adityamulik <aditya.mulik@gmail.com>
2020-11-03 14:57:10 -0500

    Created Bar Chart using D3

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,34 +1,72 @@
 import * as d3 from 'd3';
 import { schemeBrBG } from 'd3';
 
-const url = "https://udemy-react-d3.firebaseio.com/ages.json"
+const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json"
+const MARGIN = { TOP: 10, BOTTOM: 50, LEFT: 70, RIGHT: 10 }
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 
 export default class D3Chart {
     constructor(element) {      
-        console.log(d3.select(element)) 
         const svg = d3.select(element)
             .append('svg')
-                .attr("width", 500)
-                .attr("height", 500)
-        
+                .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+                .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+            .append("g")
+                .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+
         d3.json(url)
-            .then(function(agesData) {
-                console.log(agesData)
+            .then(function(data) {
+                const y = d3.scaleLinear()
+                    .domain([d3.min(data, d => d.height) * 0.95, d3.max(data, d => d.height)])
+                    .range([HEIGHT, 0])
+                
+                const x = d3.scaleBand()
+                    .domain(data.map(d => d.name))
+                    .range([0, WIDTH])
+                    .padding(0.5)
+
+                data.forEach(d => {
+                    console.log(y(d.height));
+                }) 
+
+                const xAxisCall = d3.axisBottom(x);
+                svg.append("g")
+                    .attr("transform", `translate(0, ${HEIGHT})`)
+                    .call(xAxisCall);
+
+
+                const yAxisCall = d3.axisLeft(y);
+                svg.append("g").call(yAxisCall);
+
+                svg.append("text")
+                    .attr("x", WIDTH / 2)
+                    .attr("y", HEIGHT + 50)
+                    .attr("text-anchor", "middle")
+                    .text("The world's tallest men!")
+
+                svg.append("text")
+                    .attr("x", -HEIGHT / 2)
+                    .attr("y", -50)
+                    .attr("text-anchor", "middle")
+                    .text("Height in cm")
+                    .attr("transform", "rotate(-90)")
+
+                // console.log(data)
                 const rects = svg.selectAll('rect')
-                    .data(agesData)
+                    .data(data)
 
                 rects.enter()
                     .append("rect")
-                        .attr("x", (d, i) => i * 100)
-                        .attr("y", 50)
-                        .attr("width", 50)
-                        .attr("height", d => d.age * 10)
-                        .attr("fill", d => {
-                            if(d.age > 10) {
-                                return 'red';
-                            }
-                            return "green";
-                        })
+                        .attr("x", d => x(d.name))
+                        .attr("y", d => y(d.height))
+                        // .attr("x", 0)
+                        // .attr("y", d => x(d.name))
+                        .attr("width", x.bandwidth)
+                        .attr("height", d =>HEIGHT - y(d.height))
+                        // .attr("height", x.bandwidth)
+                        // .attr("width", d => y(d.height))
+                        .attr("fill", 'grey')
             })
     }
 }
